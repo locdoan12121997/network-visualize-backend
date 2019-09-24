d0effd386dc6489bd0a8e92f8556e27be1e89fbf
Loc Doan <locdoan12121997@gmail.com>
2019-09-24 18:31:41 +0700

    init global object flask

--- api/graph.py
+++ api/graph.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request
+import igraph as ig
 
 blueprint = Blueprint('graph', __name__)
-@blueprint.route('/edges', methods=['POST'])
-def create_new_graph():
-    return "ok"
+
+@blueprint.route('/graphs', methods=['POST'])
+def upload_graph():
+    filename = request.json['filename']
+    ggraph = g.Graph.Read_GraphML('exercise/networks/NREN.graphml')
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import igraph
 
 app = Flask(__name__)
 app.register_blueprint(edge_bp)
-g = igraph.Graph()
 
 if __name__ == '__main__':
     app.run()
