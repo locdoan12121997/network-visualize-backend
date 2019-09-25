0c18c398bf2f095d85f1a8147a80b2e49df240c1
gintalk <hnamb124@gmail.com>
2019-09-25 10:47:56 +0700

    - refined to match REST standard

--- api/edge.py
+++ api/edge.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
 from igraph import Graph, Edge, EdgeSeq
 
 blueprint = Blueprint('edge', __name__)
@@ -6,59 +6,77 @@ blueprint = Blueprint('edge', __name__)
 graph_as_input = Graph.Read_GraphML("./api/mock/NREN.graphml")
 
 
-@blueprint.route('/get_edges', methods=['GET'])
-def get_edges(*args):
-    if len(args) == 0:
+@blueprint.route('/edges', methods=['GET'])
+def get_edges():
+    #   args = {
+    #       "edges": [ints]
+    #   }
+
+    args = request.json
+    edge_list = get_list(args)
+
+    if len(edge_list) == 0:
         edge_dict = edge_dictify(graph_as_input.es)
-    elif isinstance(args[0], int):
-        edge = graph_as_input.es[args[0]]
-
-        edge_dict = edge_dictify(edge)
-    elif isinstance(args[0], list) and not isinstance(args[0][0], tuple):
-        edge_list = []
-
-        for edge_id in args[0]:
-            edge_list.append(graph_as_input.es[edge_id])
-
-        edge_dict = edge_dictify(edge_list)
     else:
-        edge_list = []
-
-        for (source, target) in args[0]:
-            edge = graph_as_input.es[graph_as_input.get_eid(source, target)]
-            edge_list.append(edge)
-
         edge_dict = edge_dictify(edge_list)
 
     return jsonify(edge_dict)
 
 
-@blueprint.route('/add_edges', methods=['GET'])
-def add_edges(*args):
-    if len(args) == 0:
-        raise ValueError("expected at least one argument")
-    else:
-        graph_as_input.add_edges(args[0])
-        return "ok"
+@blueprint.route('/edges', methods=['POST'])
+def add_edges():
+    #   args = {
+    #       "edges": [{
+    #           "source": int,
+    #           "target": int
+    #        },...]
+    #    }
+
+    args = request.json
+    edge_list = get_list(args)
+    graph_as_input.add_edges(edge_list)
+
+    response = {"action": "Create", "code": "Success"}
+    return make_response(jsonify(response), 201)
 
 
-@blueprint.route('/delete_edges', methods=['GET'])
-def delete_edges(edge_list=None):
+@blueprint.route('/edges', methods=['DELETE'])
+def delete_edges():
+    #   args = {
+    #       "edges": [{
+    #           "source": int,
+    #           "target": int
+    #        },...]
+    #   }
+    #
+    #    or
+    #
+    #    args = {
+    #       "edges": [ints]
+    #    }
+
+    args = request.json
+    edge_list = get_list(args)
     graph_as_input.delete_edges(edge_list)
-    return "ok"
+
+    response = {"action": "Delete", "code": "Success"}
+    return make_response(jsonify(response), 200)
 
 
 # Local Functions
-def edge_dictify(*args):
+def edge_dictify(edge_list):
     edge_dict = {}
     attributes = graph_as_input.es.attributes()
-    edges = args[0]
 
-    if isinstance(edges, (list, EdgeSeq)):
-        for edge in edges:
-            append_edge_to_dictionary(edge_dict, attributes, edge)
-    elif isinstance(edges, Edge):
-        append_edge_to_dictionary(edge_dict, attributes, edges)
+    if len(edge_list) == 0:
+        edge_list = graph_as_input.es
+
+    for edge in edge_list:
+        if isinstance(edge, int):
+            edge = graph_as_input.es[edge]
+        elif isinstance(edge, tuple):
+            edge = graph_as_input.es[graph_as_input.get_eid(edge[0], edge[1])]
+        append_edge_to_dictionary(edge_dict, attributes, edge)
 
     return edge_dict
 
@@ -72,3 +90,21 @@ def append_edge_to_dictionary(dictionary, attributes, edge):
 
     for attr in attributes:
         dictionary[index][attr] = str(edge[attr])
+
+
+def get_list(json_obj):
+    edges = json_obj["edges"]
+
+    if len(edges) == 0:
+        return []
+
+    edge_list = []
+    if isinstance(edges[0], int):
+        for edge in edges:
+            edge_list.append(edge)
+    else:
+        for st_tuple in edges:
+            st = (st_tuple["source"], st_tuple["target"])
+            edge_list.append(st)
+
+    return edge_list
