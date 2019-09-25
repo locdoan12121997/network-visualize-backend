b03e0ff0764d402e07993315f8ef98bf87d98b99
Loc Doan <cs2015_loc.dtt@student.vgu.edu.vn>
2019-09-25 08:00:31 +0700

    temporary commit from home

--- api/graph.py
+++ api/graph.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, send_file, request, g
+from flask import Blueprint, send_file, request, Response
 import igraph as ig
 
 from util import get_graph
 
 blueprint = Blueprint('graph', __name__)
 
-@blueprint.route('/graphs', methods=['POST'])
+@blueprint.route('/graphs', methods=['GET'])
 def upload_graph():
     filename = request.json['filename']
     graph = get_graph(filename)
-    return send_file(filename), 201
+    return send_file(filename), 200
+
+
+@blueprint.route('/graphs', methods=['POST'])
+def save_graph():
+    filename = request.json['filename']
+    graph = get_graph()
+    # ig.Graph.write_graphml('static/' + filename)
+    return Response("Save succedded", 201)
